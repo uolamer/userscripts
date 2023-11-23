586d603b2b13a0bfc878fd5556b000cafc0993a2
uolamer <91064255+uolamer@users.noreply.github.com>
2023-11-23 06:03:52 -0600

    Create Helltides.com - Map Extender.user.js
    created

---
+++ Helltides.com - Map Extender.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Helltides.com - Map Extender
+// @namespace    https://helltides.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       DrainLife
+// @match        https://helltides.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=helltides.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Edit the numbers at the end of the following two lines for other sizes. Eg: 1200px
+    document.querySelector("#main > div.flex.flex-col.w-full.items-center.justify-center > div").setAttribute("style","max-height:1200px ; max-width:1500px");
+
+})();
